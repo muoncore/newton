56caf2bd40417a1a4a458620769fe5624fd6aadc
David Dawson <me@daviddawson.me>
2017-05-10 14:15:09 +0100

    Ignore sample test.

--- java/newton-sample/src/test/java/io/muoncore/newton/DemoApplicationTests.java
+++ java/newton-sample/src/test/java/io/muoncore/newton/DemoApplicationTests.java
@@ -1,12 +1,15 @@
 package io.muoncore.newton;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Ignore
 public class DemoApplicationTests {
 
 	@Test
