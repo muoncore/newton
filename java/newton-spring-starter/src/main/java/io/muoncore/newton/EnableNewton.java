0ffb1314b465031d22b6441f8fbf05bd04f74bc9
Dylan Viljoen <dylan@zynafin.co.za>
2017-05-11 13:11:48 +0200

    Remove auto mongo configuration

--- java/newton-spring-starter/src/main/java/io/muoncore/newton/EnableNewton.java
+++ java/newton-spring-starter/src/main/java/io/muoncore/newton/EnableNewton.java
@@ -3,7 +3,6 @@ package io.muoncore.newton;
 
 import io.muoncore.newton.command.CommandConfiguration;
 import io.muoncore.newton.eventsource.muon.MuonEventSourceConfiguration;
-import io.muoncore.newton.mongo.MongoConfiguration;
 import io.muoncore.newton.query.QueryConfiguration;
 import io.muoncore.newton.saga.SagaConfiguration;
 import io.muoncore.newton.utils.muon.EnableNewtonRegistrar;
@@ -26,8 +25,7 @@ import java.lang.annotation.*;
   MuonEventSourceConfiguration.class,
   CommandConfiguration.class,
   QueryConfiguration.class,
-  SagaConfiguration.class,
-  MongoConfiguration.class
+  SagaConfiguration.class
 })
 public @interface EnableNewton {
   String[] value() default {};
