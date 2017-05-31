95d062f869735b81ebf3e301684e8a5d8ee5fe53
David Dawson <me@daviddawson.me>
2017-05-31 11:04:21 +0100

    Fixes #37
    Add an Optional based alternative API.

--- java/newton-spring-starter/src/main/java/io/muoncore/newton/command/CommandResult.java
+++ java/newton-spring-starter/src/main/java/io/muoncore/newton/command/CommandResult.java
@@ -4,6 +4,7 @@ import io.muoncore.newton.NewtonEvent;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 @AllArgsConstructor
@@ -11,6 +12,20 @@ public class CommandResult {
 
   private List<NewtonEvent> events;
   private CommandFailedEvent failure;
+
+  public Optional<List<NewtonEvent>> getSuccess() {
+    if (events == null) {
+      return Optional.empty()
+    }
+    return Optional.of(events);
+  }
+
+  public Optional<CommandFailedEvent> getFailure() {
+    if (failure == null) {
+      return Optional.empty()
+    }
+    return Optional.of(failure);
+  }
 
   public CommandResult onSuccess(Consumer<List<NewtonEvent>> success) {
     if (failure == null) {
