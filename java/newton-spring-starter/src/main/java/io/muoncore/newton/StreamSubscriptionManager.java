41450f96ff33f79c8317a3663cb4872096162eac
David Dawson <me@daviddawson.me>
2017-07-06 11:23:51 +0100

    Fixes #53. Will keep reconnecting to Photon on startup until it appears in the system.

--- java/newton-spring-starter/src/main/java/io/muoncore/newton/StreamSubscriptionManager.java
+++ java/newton-spring-starter/src/main/java/io/muoncore/newton/StreamSubscriptionManager.java
@@ -11,11 +11,23 @@ public interface StreamSubscriptionManager {
      * A single instance of this subscription will run in the cluster. When one node fails, another will gain the lock and
      * run from that point onwards.
      * Tracks the current position in the event stream and will continue from that point onwards.
+     *
+     * Starts from the beginning of the stream and plays from that point. Once the stream is up to date and all cold data is played, hot data will be played in order
+     * afterwards.
+     *
+     * If this replay fails, then it will be restarted somewhere in the cluster at the same point it failed.
+     *
      * @param subscriptionName
      * @param stream
      * @param onData
      */
     void globallyUniqueSubscription(String subscriptionName, String stream, Consumer<NewtonEvent> onData);
+
+    /**
+     * As {@link #globallyUniqueSubscription(String, String, Consumer)}, with the difference that this version starts from the current HOT
+     * location in the stream, not the beginning. After it has started, it has the same behaviour as the ohter variant.
+     */
+//    void globallyUniqueSubscriptionFromNow(String subscriptionName, String stream, Consumer<NewtonEvent> onData);
 
     /**
      * Will replay to the given stream and persist the current location so that on restart/ failover
